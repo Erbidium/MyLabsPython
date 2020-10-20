7233c7c593f406227b78dbd2dec47f7de8c8e14b
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 01:33:50 +0300

    first version of lab

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from math import sqrt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+katet1 = float(input("Введіть катет: "))  # введення першого катета
+hypotenuse = float(input("Введіть гіпотенузу: "))  # Введення гіпотенузи
+katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)
+innerRadius = (katet1 + katet1 - hypotenuse) / 2
+print(innerRadius)
