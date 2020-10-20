2febc54bd27be81e757f2b1d08017752910ab8a8
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 01:46:25 +0300

    Updated main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
-katet1 = float(input("Введіть катет: "))  # введення першого катета
-hypotenuse = float(input("Введіть гіпотенузу: "))  # Введення гіпотенузи
-katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)
-innerRadius = (katet1 + katet1 - hypotenuse) / 2
-print(innerRadius)
+katet1 = float(input("Введіть катет: "))            # Введення першого катета
+hypotenuse = float(input("Введіть гіпотенузу: "))   # Введення гіпотенузи
+katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)        # Обчислення другого катета
+innerRadius = (katet1 + katet2 - hypotenuse) / 2    # Обчислення радіса кола, вписаного у цей прямокутний трикутник
+print(innerRadius)                                  # Виведення обчисленого радусу на екран
