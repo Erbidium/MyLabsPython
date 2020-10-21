94d43929ad00e84ab05450074c1bdcbdbddcda0b
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 14:58:36 +0300

    Added checks

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,7 +1,13 @@
-from math import sqrt                     # Підключення функції sqrt модуля math
+from math import sqrt
 
-katet1 = float(input("Введіть катет: "))  # Введення першого катета
-hypotenuse = float(input("Введіть гіпотенузу: "))  # Введення гіпотенузи
-katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)  # Обчислення другого катета
-innerRadius = (katet1 + katet2 - hypotenuse) / 2  # Обчислення радіса кола, вписаного у цей прямокутний трикутник
-print("Радіус кола, вписаного у цей трикутник: %.3f" % innerRadius)  # Виведення обчисленого радіуса на екран
+katet1 = float(input("Введіть катет: "))
+if katet1<=0:
+    print("Трикутник с таким катетом не існує")
+else:
+    hypotenuse = float(input("Введіть гіпотенузу: "))
+    if hypotenuse<katet1:
+        print("Такий прямокутний трикутник не існує")
+    else:
+        katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)
+        innerRadius = (katet1 + katet2 - hypotenuse) / 2
+        print("Радіус кола, вписаного у цей трикутник: %.2f" % innerRadius)
