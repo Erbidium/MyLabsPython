ca6c75cc9aab455654f827c00230a19fddcc8f77
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-28 01:51:30 +0200

    Lab 1 input validation, edited comments

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,13 +1,20 @@
 from math import sqrt
+import re
 
-katet1 = float(input("Введіть катет: "))
-if katet1<=0:
-    print("Трикутник с таким катетом не існує")
+katet1 = (input('Enter a cathetus: '))
+if not (re.fullmatch(r'^[+-]?(\d*[.])?\d+$', katet1) or re.fullmatch(r'^[+-]?\d+[.]?$', katet1)):
+    print('Your data is incorrect!')
+elif float(katet1) <= 0:
+    print('The triangle with such cathetus doesn\'t exist!')
 else:
-    hypotenuse = float(input("Введіть гіпотенузу: "))
-    if hypotenuse<katet1:
-        print("Такий прямокутний трикутник не існує")
+    katet1 = float(katet1)
+    hypotenuse = (input('Enter a hypotenuse: '))
+    if not (re.fullmatch(r'^[+-]?(\d*[.])?\d+$', hypotenuse) or re.fullmatch(r'^[+-]?\d+[.]?$', hypotenuse)):
+        print('Your data is incorrect!')
+    elif float(hypotenuse < katet1):
+        print('Such right triangle doesn\'t exist!')
     else:
+        hypotenuse = float(hypotenuse)
         katet2 = sqrt(hypotenuse ** 2 - katet1 ** 2)
         innerRadius = (katet1 + katet2 - hypotenuse) / 2
-        print("Радіус кола, вписаного у цей трикутник: %.2f" % innerRadius)
+        print('The radius of the circle inscribed in this triangle: %.2f' % innerRadius)
