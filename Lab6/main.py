3ca45504455a53e5a0a2f7d7ea28d883be1ba9c6
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-12-02 14:34:42 +0200

    Changed name of the function

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,16 +1,13 @@
 from math import pi
 
 
-def minimal(num1, num2):
-    if num1 < num2:
-        return num1
-    else:
-        return num1
+def min(num1, num2):
+    return num1 if num1 < num2 else num2
 
 
 a = float(input('Please, enter number a: '))
 b = float(input('Please, enter number b: '))
-U = minimal(a, b)
-V = minimal(a * b, a + b)
+U = min(a, b)
+V = min(a * b, a + b)
 W = min(U + V ** 2, pi)
 print('\nU=%.2f\nV=%.2f\nW=%.2f' % (U, V, W))
