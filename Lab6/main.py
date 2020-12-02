76f7f2936907d23dec43d6f5d524671b9640f937
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-12-02 02:30:43 +0200

    Edited errors

--- Lab6/main.py
+++ Lab6/main.py
@@ -2,7 +2,7 @@ from math import pi
 
 
 def minimal(num1, num2):
-    if num1 > num2:
+    if num1 < num2:
         return num1
     else:
         return num1
@@ -13,4 +13,4 @@ b = float(input('Please, enter number b: '))
 U = minimal(a, b)
 V = minimal(a * b, a + b)
 W = min(U + V ** 2, pi)
-print('U=%.2f\nV=%.2f\nW=%.2f' % (U, V, W))
+print('\nU=%.2f\nV=%.2f\nW=%.2f' % (U, V, W))
