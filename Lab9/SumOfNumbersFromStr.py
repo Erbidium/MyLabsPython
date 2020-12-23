f9bffa6b7e14445eceb3db888e3d8b6fc0d62ae9
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-12-23 15:18:26 +0200

    Formatted the code

--- Lab9/SumOfNumbersFromStr.py
+++ Lab9/SumOfNumbersFromStr.py
@@ -2,11 +2,11 @@ def getSumOfNumbersFromStr(strWithNumbers):
     currentNumber = '0'
     sum = 0
     for i in range(0, len(strWithNumbers)):
-        if(strWithNumbers[i].isdigit()==True):
+        if strWithNumbers[i].isdigit():
             currentNumber += strWithNumbers[i]
         else:
             sum += int(currentNumber)
             currentNumber = '0'
-        if(i==len(strWithNumbers)-1):
+        if i==len(strWithNumbers)-1:
             sum += int(currentNumber)
     return sum
--- Lab9/main.py
+++ Lab9/main.py
@@ -3,3 +3,9 @@ from SumOfNumbersFromStr import getSumOfNumbersFromStr
 strWithNumbers = input('Please, enter a string: ')
 sum = getSumOfNumbersFromStr(strWithNumbers)
 print('Sum of numbers in string: ', sum)
+
+
+
+
+
+
